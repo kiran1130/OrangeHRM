70166115a3c110833a81799e33e0aa33982b48f2
Kirankumar K <kirankhsn1@rediffmail.com>
2023-11-23 18:34:18 +0530

    Changed demo.java file

--- src/test/java/tests/DemoTest.java
+++ src/test/java/tests/DemoTest.java
@@ -5,7 +5,7 @@ public class DemoTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("My new test");
+		System.out.println("My new test new branch");
 
 	}
 
